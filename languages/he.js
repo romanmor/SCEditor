f9f186d1a7192f9fad613608c844e7458c29ab63
Roman Moratov <31098310+romanmor@users.noreply.github.com>
2021-11-19 16:22:25 +0200

    Create he.js

---
+++ languages/he.js
@@ -0,0 +1,75 @@
+/**
+ * @author Roman Moratov www.DevZ.co.il
+ * @license [MIT](http://www.opensource.org/licenses/mit-license.php)
+ */
+(function () {
+	'use strict';
+
+	// Replace <code> with the language code, e.g. no, fr, en, ect.
+	sceditor.locale['he'] = {
+
+		// Original string is on the left, place the translation between
+		// the quotes on the right
+		'Bold': 'שמן',
+		'Italic': 'נטוי',
+		'Underline': 'קו תחתון',
+		'Strikethrough': 'קו חצוי',
+		'Subscript': כתב תחתון'',
+		'Superscript': 'כתב עליון',
+		'Align left': 'הפנה שמאלה',
+		'Center': 'מרכז',
+		'Align right': 'ימינה',
+		'Justify': 'יישור',
+		'Font Name': 'שם הפונט',
+		'Font Size': 'גודל פונט',
+		'Font Color': 'צבע פונט',
+		'Remove Formatting': 'מחק פורמט',
+		'Cut': 'גזור',
+		'Your browser does not allow the cut command. Please use the keyboard shortcut Ctrl/Cmd-X': 'הדפדפן שלך לא תומך, לחץ על Ctrl/Cmd+X',
+		'Copy': 'העתק',
+		'Your browser does not allow the copy command. Please use the keyboard shortcut Ctrl/Cmd-C': 'הדפדפן שלך לא תומך, לחץ על Ctrl/Cmd+C',
+		'Paste': '',
+		'Your browser does not allow the paste command. Please use the keyboard shortcut Ctrl/Cmd-V': 'הדפדפן שלך לא תומך, לחץ על Ctrl/Cmd+V',
+		'Paste your text inside the following box:': 'הדבק את טקסט לכאן',
+		'Paste Text': 'הדבק טקסט',
+		'Bullet list': '',
+		'Numbered list': '',
+		'Undo': '',
+		'Redo': '',
+		'Rows:': '',
+		'Cols:': '',
+		'Insert a table': '',
+		'Insert a horizontal rule': '',
+		'Code': '',
+		'Width (optional):': '',
+		'Height (optional):': '',
+		'Insert an image': '',
+		'E-mail:': '',
+		'Insert an email': '',
+		'URL:': '',
+		'Insert a link': '',
+		'Unlink': '',
+		'More': '',
+		'Insert an emoticon': '',
+		'Video URL:': '',
+		'Insert': '',
+		'Insert a YouTube video': '',
+		'Insert current date': '',
+		'Insert current time': '',
+		'Print': '',
+		'View source': '',
+		'Description (optional):': '',
+		'Enter the image URL:': '',
+		'Enter the e-mail address:': '',
+		'Enter the displayed text:': '',
+		'Enter URL:': '',
+		'Enter the YouTube video URL or ID:': '',
+		'Insert a Quote': '',
+		'Invalid YouTube video': '',
+		'Drop files here': '',
+
+		// month format, replace - with the date format separator and order in the
+		// order used
+		dateFormat: 'day-month-year'
+	};
+})();
